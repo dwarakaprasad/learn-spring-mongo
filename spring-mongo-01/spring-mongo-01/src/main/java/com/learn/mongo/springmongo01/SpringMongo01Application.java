100be1bd53c0e226550d0c2a3f91590e5b10100c
dzddh16264 <dzddh16264@NYXALBDSK031.nys.local>
2019-05-22 15:00:23 -0400

    Initial Commit

---
+++ spring-mongo-01/spring-mongo-01/src/main/java/com/learn/mongo/springmongo01/SpringMongo01Application.java
@@ -0,0 +1,13 @@
+package com.learn.mongo.springmongo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongo01Application.class, args);
+	}
+
+}
