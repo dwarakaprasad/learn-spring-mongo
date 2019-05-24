0a7ea1763404874a4d8dbfa7bb6ecbd9877ce317
Dwarakaprasad Thakku Damodaran <dwarka.damodaran@gmail.com>
2019-05-24 11:35:15 -0400

    Add files via upload

---
+++ spring-mongo-dbRef/src/main/java/com/learn/mongo/springmongodbRef/SpringMongoDbRefApplication.java
@@ -0,0 +1,13 @@
+package com.learn.mongo.springmongodbRef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoDbRefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoDbRefApplication.class, args);
+	}
+
+}
---
+++ spring-mongo-dbRef/src/test/java/com/learn/mongo/springmongodbRef/SpringMongoDbRefApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learn.mongo.springmongodbRef;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMongoDbRefApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
